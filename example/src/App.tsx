6ab0898e7dfb2bb308b2ee7ec46ecb4ef2ed30f4
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2023-08-16 15:26:56 +0600

    build: add basic cone slider

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,70 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-cone-slider';
+import { Text, View } from 'react-native';
+import { ConeSlider } from 'react-native-cone-slider';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [sliderValue, setSliderValue] = React.useState(0);
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const handleValueChange = (value: number) => {
+    setSliderValue(value);
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <View>
+        <Text
+          style={{
+            fontSize: 24,
+            marginBottom: 10,
+            color: 'black',
+            textAlign: 'center',
+          }}
+        >
+          Slider Value: {sliderValue.toFixed(2)}
+        </Text>
+      </View>
+
+      <View>
+        <ConeSlider
+          height={100}
+          width={200}
+          min={0}
+          max={100}
+          onValueChange={handleValueChange}
+          containerStyle={{
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: 100,
+          }}
+        />
+      </View>
     </View>
   );
+  // const [result, setResult] = React.useState<number | undefined>();
+
+  // React.useEffect(() => {
+  //   multiply(3, 7).then(setResult);
+  // }, []);
+
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Result: {result}</Text>
+  //   </View>
+  // );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   box: {
+//     width: 60,
+//     height: 60,
+//     marginVertical: 20,
+//   },
+// });
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,108 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import React from 'react';
+import { View } from 'react-native';
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+  type PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
+import Animated, {
+  runOnJS,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
+import { clamp } from 'react-native-redash';
+interface ConeSliderProps {
+  width: number;
+  height: number;
+  min: number;
+  max: number;
+  onValueChange?: (value: number) => void;
+  containerStyle?: Object;
 }
+type GestureContext = {
+  startX: number;
+};
+
+export const ConeSlider: React.FC<ConeSliderProps> = ({
+  width,
+  height,
+  min,
+  max,
+  onValueChange,
+  containerStyle,
+}) => {
+  const translateX = useSharedValue(0);
+  const range = max - min;
+
+  const onValueChangeSafe = (value: number) => {
+    if (onValueChange && typeof onValueChange === 'function') {
+      onValueChange(value);
+    }
+  };
+
+  const gestureHandler = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    GestureContext
+  >({
+    onActive: (event, ctx) => {
+      translateX.value = clamp(ctx.startX + event.translationX, 0, width - 30);
+      runOnJS(onValueChangeSafe)(
+        (translateX.value / (width - 30)) * range + min
+      );
+    },
+    onStart: (_, ctx) => {
+      ctx.startX = translateX.value;
+    },
+  });
+
+  const circleStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: translateX.value }],
+  }));
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View
+        style={[
+          { flex: 1, justifyContent: 'center', alignItems: 'center' },
+          containerStyle,
+        ]}
+      >
+        <View style={{ height, overflow: 'hidden', width }}>
+          <View
+            style={{
+              backgroundColor: 'transparent',
+              borderBottomColor: 'transparent',
+              borderBottomWidth: height / 6,
+              borderLeftColor: 'red',
+              borderLeftWidth: 0,
+              borderRightColor: '#CCCCCC',
+              borderRightWidth: width,
+              borderStyle: 'solid',
+              borderTopColor: 'transparent',
+              borderTopWidth: height / 7,
+              height: 0,
+              width: 0,
+            }}
+          />
+
+          <PanGestureHandler onGestureEvent={gestureHandler}>
+            <Animated.View
+              style={[
+                {
+                  backgroundColor: 'grey',
+                  borderRadius: 25,
+                  height: 30,
+                  position: 'absolute',
+                  top: height / 4 - 25,
+                  width: 30,
+                },
+                circleStyle,
+              ]}
+            />
+          </PanGestureHandler>
+        </View>
+      </View>
+    </GestureHandlerRootView>
+  );
+};
